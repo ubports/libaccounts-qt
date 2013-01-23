784b962824e715e49239c91bfa52bd2d91c820c4
Alberto Mardegan <alberto.mardegan@canonical.com>
2013-01-23 14:56:30 +0100

    Tests: use dbus-test-runner if available

--- tests/accountstest.sh
+++ tests/accountstest.sh
@@ -6,4 +6,12 @@ export AG_SERVICE_TYPES=$TESTDIR
 export AG_PROVIDERS=$TESTDIR
 export ACCOUNTS=/tmp/ 
 
-$WRAPPER ./accountstest "$@"
+# If dbus-test-runner exists, use it to run the tests in a separate D-Bus
+# session
+if command -v dbus-test-runner > /dev/null ; then
+	echo "Using dbus-test-runner"
+	dbus-test-runner -m 180 -t ./accountstest
+else
+	echo "Using existing D-Bus session"
+	$WRAPPER ./accountstest "$@"
+fi
