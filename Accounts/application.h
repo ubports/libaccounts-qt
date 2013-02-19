fb68e3eff58d4d1685df7a84c295a4ebc640725c
Alberto Mardegan <alberto.mardegan@canonical.com>
2013-02-19 17:14:35 +0400

    Application: fix typo in variable name

--- Accounts/application.h
+++ Accounts/application.h
@@ -55,7 +55,7 @@ public:
 private:
     // Don't include private data in docs: \cond
     friend class Manager;
-    Application(AgApplication *authData);
+    Application(AgApplication *application);
 
     AgApplication *m_application;
     // \endcond
